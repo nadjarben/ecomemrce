0681030506c394cc0f911a18afa95dc15ae75424
nadjarben <nadjarbenj@gmail.com>
2020-04-17 15:55:43 +0300

    implement redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,33 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
-import { ReactComponent as Logo } from "../../assets/4.4 crown.svg.svg";
 
-import "./header.styles.scss";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = ({ currentUser }) => {
-  return (
-    <div className="header">
-      <Link className="logo-container" to="/">
-        <Logo className="logo" />
+import './header.styles.scss';
+
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
       </Link>
-      <div className="options">
-          <Link to="/shop" className="option">
-              SHOP
-          </Link>
-          <Link to="/shop" className="option">
-              CONTACT
-          </Link>
-          {
-            currentUser ?
-            <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-            :
-            <Link className="option" to="/signin">SIGN IN</Link>
-          }
-      </div>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
-  );
-};
+  </div>
+);
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
